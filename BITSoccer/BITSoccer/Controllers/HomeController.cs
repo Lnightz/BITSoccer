969c4582077c2286c4b13f9fc3daf0f707716bc5
Lnightz <lightzzero@gmail.com>
2020-06-14 20:39:27 +0700

    Update

--- BITSoccer/BITSoccer/Controllers/HomeController.cs
+++ BITSoccer/BITSoccer/Controllers/HomeController.cs
@@ -60,78 +60,10 @@ namespace BITSoccer.Controllers
         //{
         //    return View();
         //}
-        public ActionResult AddToCart(int? id)
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                if (User.IsInRole("Student"))
-                {
-                    var userdetails = db.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
-                    ViewBag.UserDetails = userdetails;
-                    if (id == null)
-                    {
-                        return View("Index");
-                    }
-                    var classdetails = db.Classes.Where(x => x.Class_ID == id).FirstOrDefault();
-                    if (classdetails == null)
-                    {
-                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                    }
-                    var cart = Session["ClassCart"];
+        
 
-                    if (cart == null)
-                    {
-                        List<Cart> listcarts = new List<Cart>
-                        {
-                            new Cart(db.Users.Find(userdetails.User_ID),db.Classes.Find(id))
-                        };
-                        Session["ClassCart"] = listcarts;
-                    }
-                    else
-                    {
-                        List<Cart> listcarts = (List<Cart>)Session["ClassCart"];
-                        int check = IsExstingCheck(id);
-                        if (check == -1)
-                        {
-                            listcarts.Add(new Cart(db.Users.Find(userdetails.User_ID), db.Classes.Find(id)));
-                        }
-                    }
-                    return View();
-                }
-                else
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-            }
 
-            return RedirectToAction("Index", "Home");
-        }
 
-        //Kiểm tra xem có tồn tại Cart trước đó không
-        private int IsExstingCheck (int? id)
-        {
-            List<Cart> listcarts = (List<Cart>)Session["ClassCart"];
-            for (int i = 0; i< listcarts.Count; i++)
-            {
-                if (listcarts[i].Class.Class_ID == id) return i;
-            }
-            return -1;
-        }
-
-        //Xoá lớp ra khỏi cart
-
-        public ActionResult DeleteCart (int? id)
-        {
-            var userdetails = db.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
-            ViewBag.UserDetails = userdetails;
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
-            int check = IsExstingCheck(id);
-            List<Cart> listcarts = (List<Cart>)Session["ClassCart"];
-            listcarts.RemoveAt(check);
-            return View("AddToCart");
-        }
+   
     }
 }
